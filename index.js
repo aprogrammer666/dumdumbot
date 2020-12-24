b817139686d58b43a6aee84add4dd5388857f1f0
aprogrammer666 <55362687+aprogrammer666@users.noreply.github.com>
2020-12-24 20:11:58 +0300

    some fixes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js')
-const client = new Discord.client()
+const client = new Discord.Client()
 // const config = require('./config.json')
 
 client.on('ready', () => {
-    console.log('Hazırım kaptan!')
+    console.log('Hazirim kaptan!')
 })
 
 client.login(process.env.djs_token)
