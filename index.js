cb688dd701894995c58ff7c8122320fe0fc0e888
aprogrammer666 <55362687+aprogrammer666@users.noreply.github.com>
2020-12-24 19:39:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+const client = new Discord.client()
+const config = require('./config.json')
+
+client.on('ready', () => {
+    console.log('Hazırım kaptan!')
+})
+
+client.login(config.token)
