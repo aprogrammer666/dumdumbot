e911bce2fa74738d6ee3d9463ade006a2b494373
aprogrammer666 <55362687+aprogrammer666@users.noreply.github.com>
2021-02-12 20:05:17 +0300

    commands

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 // const config = require('./config.json')
+const command = require('./commands')
 
 client.on('ready', () => {
     console.log('Hazirim kaptan!')
 })
+
+command(client, 'ping', message=> {
+    message.channel.send('Pong!')
+})
+
 client.on('message', msg=> {
     if(msg.content === 'sa') {
         msg.reply('as.');
