0e0046fd15a54fdb2c1bcecf763b61de86385f51
aprogrammer666 <55362687+aprogrammer666@users.noreply.github.com>
2021-02-13 13:49:39 +0300

    .botbilgisi fix

--- index.js
+++ index.js
@@ -2,44 +2,47 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 // const config = require('./config.json')
 const command = require('./command')
+const firstMessage = require('./firstmessage.js')
 
 client.on('ready', () => {
     console.log('Hazirim kaptan!')
+    firstMessage(client, '', 'hello world', [''])
+
+    
+  command(client, 'ping', (message) => {
+      message.channel.send('Pong!')
+  })
+  
+  command(client, 'botbilgisi', (message) => {
+      message.channel.send('**Şu sunucularda bulunuyorum**')
+      client.guilds.cache.forEach((guild) => {     
+          message.channel.send(
+              `>> ${guild.name} toplamda ${guild.memberCount} üyeye sahip.`
+          )
+      })
+  })
+  
+  command(client, 'toplusil', (message) => {
+      if (message.member.hasPermission('ADMINISTRATOR')) {
+          message.channel.messages.fetch().then(results => {
+              console.log(results)
+              message.channel.bulkDelete(results)
+          })
+      }
+  })
+  
+  command(client, 'status', (message) => {
+      const content = message.content.replace('.status ', '')
+  
+      client.user.setPresence({
+          activity: {
+              name: content,
+              type: 0,
+  
+          }
+      })
+  } )
 })
- 
-command(client, 'ping', (message) => {
-    message.channel.send('Pong!')
-})
-
-command(client, 'botbilgisi', (message) => {
-    client.guilds.cache.forEach((guild) => {
-        message.channel.send('**Şu sunucularda bulunuyorum**')
-        message.channel.send(
-            `>> ${guild.name} toplamda ${guild.memberCount} üyeye sahip.`
-        )
-    })
-})
-
-command(client, 'toplusil', (message) => {
-    if (message.member.hasPermission('ADMINISTRATOR')) {
-        message.channel.messages.fetch().then(results => {
-            console.log(results)
-            message.channel.bulkDelete(results)
-        })
-    }
-})
-
-command(client, 'status', (message) => {
-    const content = message.content.replace('.status ', '')
-
-    client.user.setPresence({
-        activity: {
-            name: content,
-            type: 0,
-
-        }
-    })
-} )
 
 
 client.login(process.env.djs_token)
