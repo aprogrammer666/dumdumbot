da96c30c891d259d0c87974c7b867c6b647cf815
aprogrammer666 <55362687+aprogrammer666@users.noreply.github.com>
2021-02-15 23:44:04 +0300

    sending error log to dev #1

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const command = require('./command')
 
 client.on('ready', () => {
     console.log('Hazirim kaptan!') // Bot aktif olunca konsolda bize haber veren mesaj.
+});
+// 'sa' yazanlara karşılık olarak 'as' cevabını veriyor.
+client.on('message', function(message) {
+    if(message.content.toLowerCase === 'sa') {
+        message.channel.send('as');
+    }
+client.on('error', function(error) {
+    const botdev = '274235277969588225'
+    message.author1.send(error)
 })
-
+});
   // .ping >> Pong!   | Made for test purposes.  
   command(client, 'ping', (message) => {
       message.channel.send('Pong!') 
@@ -18,17 +27,17 @@ client.on('ready', () => {
           message.channel.send(
               `>> ${guild.name} toplamda ${guild.memberCount} üyeye sahip.`
           )
-      })
-  })
+      });
+  });
   
   command(client, 'toplusil', (message) => {
       if (message.member.hasPermission('ADMINISTRATOR')) {
           message.channel.messages.fetch().then(results => {
               console.log(results)
               message.channel.bulkDelete(results)
-          })
+          });
       }
-  })
+  });
   // ".status <input>" şeklinde botun durumunu ayarlamamızı sağlayan komut.
   command(client, 'status', (message) => {
       const content = message.content.replace('.status ', '')
@@ -39,7 +48,7 @@ client.on('ready', () => {
               type: 0,
   
           }
-      })
+      });
   } )
 
 
