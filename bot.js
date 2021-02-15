c869bc89b167079add034b86a63a01a7d6192137
aprogrammer666 <55362687+aprogrammer666@users.noreply.github.com>
2021-02-16 00:13:51 +0300

    index.js >> bot.js

---
+++ bot.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+// const config = require('./config.json')
+const command = require('./command')
+
+client.on('ready', function(message) {
+    const botdev = '274235277969588225'
+    console.log('Hazirim kaptan!') // Bot aktif olunca konsolda bize haber veren mesaj.
+    message.botdev.sendMessage('Hazırım kaptan!')
+});
+// 'sa' yazanlara karşılık olarak 'as' cevabını veriyor.
+client.on('message', function(message) {
+    if(message.content.toLowerCase === 'sa') {
+        message.channel.send('as');
+    }
+});
+
+
+// .ping >> Pong!   | Made for test purposes.  
+command(client, 'ping', (message) => {
+    message.channel.send('Pong!') 
+})
+// Botun dahil olduğu sunucular ve o sunuculardaki üye sayıları hakkında bilgi verir.
+command(client, 'botbilgisi', (message) => {
+    message.channel.send('**Şu sunucularda bulunuyorum**')
+    client.guilds.cache.forEach((guild) => {     
+        message.channel.send(
+            `>> ${guild.name} toplamda ${guild.memberCount} üyeye sahip.`
+        )
+    });
+});
+
+command(client, 'toplusil', (message) => {
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+        message.channel.messages.fetch().then(results => {
+            console.log(results)
+            message.channel.bulkDelete(results)
+        });
+    }
+});
+// ".status <input>" şeklinde botun durumunu ayarlamamızı sağlayan komut.
+command(client, 'status', (message) => {
+    const content = message.content.replace('.status ', '')
+
+    client.user.setPresence({
+        activity: {
+            name: content,
+            type: 0,
+
+        }
+    });
+} )
+
+// Hayır, tokenimi çalamazsınız ;)
+client.login(process.env.djs_token)
