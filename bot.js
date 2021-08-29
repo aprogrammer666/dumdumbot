c8d96f43feaca5ffd9eb4a810f815b9b2141ce9f
aprogrammer666 <55362687+aprogrammer666@users.noreply.github.com>
2021-08-30 00:28:50 +0300

    updating code to v13 #2

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const client = new Discord.Client()
 const config = require('./config.json')
 const command = require('./command')
 const ytdl = require('ytdl-core')
